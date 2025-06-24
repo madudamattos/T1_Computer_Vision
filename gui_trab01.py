7410d052b10cd0f1f673cff1cdab16745530bfb1
maduda <madudamattos>
2025-06-24 19:56:13 -0300

    ajuste nos limites do grafico 3d

--- gui_trab01.py
+++ gui_trab01.py
@@ -9,135 +9,6 @@ from numpy import array
 from stl import mesh
 
 ###### Crie suas funções de translação, rotação, criação de referenciais, plotagem de setas e qualquer outra função que precisar
-# Função para ajustar o eixo de coordenadas do objeto 3D em relação à mudança de posição ou orientação da câmera
-def set_view_object_cam(ax):
-
-    x_lim= ax.get_xlim3d()
-    y_lim = ax.get_ylim3d()
-    z_lim = ax.get_zlim3d()
-
-    x_ran = abs(np.diff(x_lim))
-    x_mean = np.mean(x_lim)
-    y_ran = abs(np.diff(y_lim))
-    y_mean = np.mean(y_lim)
-    z_ran = abs(np.diff(z_lim))
-    z_mean = np.mean(z_lim)
-
-    plot_radius = 0.4*max([x_ran, y_ran, z_ran])
-
-    ax.set_xlim3d([x_mean - plot_radius, x_mean + plot_radius])
-    ax.set_ylim3d([y_mean - plot_radius, y_mean + plot_radius])
-    ax.set_zlim3d([z_mean - plot_radius, z_mean + plot_radius])
-
-# funçõe de criação de Objeto 3D
-def defineObj():
-    # Objeto 3D 
-    pokemon = mesh.Mesh.from_file('models/poliwag.stl')
-
-    # Transforma as coordenadas
-    x = pokemon.x.flatten()
-    y = pokemon.y.flatten()
-    z = pokemon.z.flatten()
-
-    # Esse é o objeto 3D em coordenadas homogeneas
-    pokemon = np.array([x.T, y.T, z.T, np.ones(x.size)])
-
-    # Ajustar o obj inicial
-    M = np.array([[4, 0, 0, 0], [0, 4, 0, 0], [0, 0, 4, 0], [0,0,0,1]])
-    
-    R = z_rotate(20)
-
-    R = y_rotate(20)@ R
-    
-    pokemon = R@pokemon
-
-    pokemon = M@pokemon
-
-    return pokemon
-
-# Funçõe pra convertir valor de interface
-def convert_value(text):
-    if text == "": return float(0)
-    return float(text)
-
-# Funções de rotação
-def x_rotate(angle_x):
-    return np.array([[1,0,0,0],
-                        [0, np.cos(np.deg2rad(angle_x)),-np.sin(np.deg2rad(angle_x)),0],
-                        [0, np.sin(np.deg2rad(angle_x)), np.cos(np.deg2rad(angle_x)),0],
-                        [0,0,0,1]])
-def y_rotate(angle_y):
-    return np.array([[np.cos(np.deg2rad(angle_y)),0, np.sin(np.deg2rad(angle_y)),0],
-                        [0,1,0,0],
-                        [-np.sin(np.deg2rad(angle_y)), 0, np.cos(np.deg2rad(angle_y)),0],
-                        [0,0,0,1]])
-def z_rotate(angle_z):
-    return np.array([[np.cos(np.deg2rad(angle_z)),-np.sin(np.deg2rad(angle_z)),0,0],
-                        [np.sin(np.deg2rad(angle_z)),np.cos(np.deg2rad(angle_z)),0,0],
-                        [0,0,1,0],
-                        [0,0,0,1]])
-
-def x_rotate_cam(xangle, M_x):
-    M_inv = np.linalg.inv(M_x)
-    cam_orig = np.dot(M_inv, M_x)
-    Rx = np.array([[1,0,0,0],
-                [0, np.cos(np.deg2rad(xangle)),-np.sin(np.deg2rad(xangle)),0],
-                [0, np.sin(np.deg2rad(xangle)), np.cos(np.deg2rad(xangle)),0],
-                [0,0,0,1]])
-    M_RxO = Rx@cam_orig
-    M_Rx = M_x@M_RxO
-    return M_Rx
-    
-def y_rotate_cam(yangle, M_y):
-    M_inv = np.linalg.inv(M_y)
-    cam_orig = np.dot(M_inv, M_y)
-    Ry = np.array([[np.cos(np.deg2rad(yangle)),0, np.sin(np.deg2rad(yangle)),0],
-                    [0,1,0,0],
-                    [-np.sin(np.deg2rad(yangle)), 0, np.cos(np.deg2rad(yangle)),0],
-                    [0,0,0,1]])
-    M_RyO = cam_orig@Ry
-    M_Ry = M_y@M_RyO
-    return M_Ry
-    
-def z_rotate_cam(zangle, M_z):
-    M_inv = np.linalg.inv(M_z)
-    cam_orig = np.dot(M_inv, M_z)
-    Rz = np.array([[np.cos(np.deg2rad(zangle)),-np.sin(np.deg2rad(zangle)),0,0],
-                    [np.sin(np.deg2rad(zangle)),np.cos(np.deg2rad(zangle)),0,0],
-                    [0,0,1,0],
-                    [0,0,0,1]])
-    M_RzO = cam_orig@Rz
-    M_Rz = M_z@M_RzO
-    return M_Rz
-
-# Funções de translação
-def translate(x,y,z):
-    return np.array([[1,0,0,x],
-                        [0,1,0,y],
-                        [0,0,1,z],
-                        [0,0,0,1]])
-    
-
-def translate_cam(x,y,z, M_T):
-    M_inv_T = np.linalg.inv(M_T)
-    cam_orig = np.dot(M_inv_T, M_T)
-    T = np.array([[1,0,0,x],
-                    [0,1,0,y],
-                    [0,0,1,z],
-                    [0,0,0,1]])
-    M_TO = cam_orig@T
-    M_acc = M_T@M_TO
-    return M_acc
-
-# função emprestada do material da professora
-def draw_arrows(point,base,axis,length=0.5):
-    # Plot vector of x-axis
-    qx = axis.quiver(point[0],point[1],point[2],base[0,0],base[1,0],base[2,0],color='red',pivot='tail',  length=length)
-    # Plot vector of y-axis
-    qy = axis.quiver(point[0],point[1],point[2],base[0,1],base[1,1],base[2,1],color='green',pivot='tail',  length=length)
-    # Plot vector of z-axis
-    qz = axis.quiver(point[0],point[1],point[2],base[0,2],base[1,2],base[2,2],color='blue',pivot='tail',  length=length)
-    return [qx, qy, qz]
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -404,23 +275,20 @@ class MainWindow(QMainWindow):
     def plotWorld(self):
         # base vector values
         base = np.array([
-                         [1, 0, 0],  # eixo X
-                         [0, 1, 0],  # eixo Y
-                         [0, 0, 1],  # eixo Z
-                         [0, 0, 0]   # coordenada homogênea
+                         [1, 0, 0],  
+                         [0, 1, 0],  
+                         [0, 0, 1],  
+                         [0, 0, 0]   
                     ]) 
 
         # origin point
         origin = np.array([[0],[0],[0],[1]])
 
         draw_arrows(origin,base,self.ax2,3)
-
         return 
     
     def plotCam(self):
         draw_arrows(self.cam[:, -1], self.cam[:, 0:3], self.ax2,3)
-
-
         return
 
     def update_params_intrinsc(self, line_edits):
@@ -567,25 +435,165 @@ class MainWindow(QMainWindow):
         
         
         self.ax2.cla()
-        self.ax2.set_title("Objeto")  
-        # Plot do referencial do mundo
+        self.ax2.set_title("Objeto") 
+
         self.plotWorld()
-        # Plot da câmera 
         self.plotCam()
-        # Plot do objeto 3D
         self.plotObj()
-        # Configurando o eixo 3D
+
         set_view_object_cam(self.ax2)
+
         self.fig2.canvas.draw_idle()
         self.fig2.canvas.flush_events()
         return 
 
     def reset_canvas(self):
-        # volta as variáveis para o valor original
+        # Volta as variáveis para o valor original
         self.set_variables()
+
+        # Atualiza o canva
         self.update_canvas()
         return
     
+# funçõe de criação de Objeto 3D
+def defineObj():
+    # Objeto 3D 
+    pokemon = mesh.Mesh.from_file('models/poliwag.stl')
+
+    # Transforma as coordenadas
+    x = pokemon.x.flatten()
+    y = pokemon.y.flatten()
+    z = pokemon.z.flatten()
+
+    # Esse é o objeto 3D em coordenadas homogeneas
+    pokemon = np.array([x.T, y.T, z.T, np.ones(x.size)])
+
+    # Ajustar o obj inicial
+    M = np.array([[4, 0, 0, 0], [0, 4, 0, 0], [0, 0, 4, 0], [0,0,0,1]])
+    
+    R = z_rotate(20)
+
+    R = y_rotate(20)@ R
+    
+    pokemon = R@pokemon
+
+    pokemon = M@pokemon
+
+    return pokemon
+
+# Funçõe pra convertir valor de interface
+def convert_value(text):
+    if text == "": return float(0)
+    return float(text)
+
+# Funções de rotação
+def x_rotate(angle_x):
+    return np.array([[1,0,0,0],
+                        [0, np.cos(np.deg2rad(angle_x)),-np.sin(np.deg2rad(angle_x)),0],
+                        [0, np.sin(np.deg2rad(angle_x)), np.cos(np.deg2rad(angle_x)),0],
+                        [0,0,0,1]])
+def y_rotate(angle_y):
+    return np.array([[np.cos(np.deg2rad(angle_y)),0, np.sin(np.deg2rad(angle_y)),0],
+                        [0,1,0,0],
+                        [-np.sin(np.deg2rad(angle_y)), 0, np.cos(np.deg2rad(angle_y)),0],
+                        [0,0,0,1]])
+def z_rotate(angle_z):
+    return np.array([[np.cos(np.deg2rad(angle_z)),-np.sin(np.deg2rad(angle_z)),0,0],
+                        [np.sin(np.deg2rad(angle_z)),np.cos(np.deg2rad(angle_z)),0,0],
+                        [0,0,1,0],
+                        [0,0,0,1]])
+
+def x_rotate_cam(xangle, M_x):
+    M_inv = np.linalg.inv(M_x)
+    cam_orig = np.dot(M_inv, M_x)
+    Rx = np.array([[1,0,0,0],
+                [0, np.cos(np.deg2rad(xangle)),-np.sin(np.deg2rad(xangle)),0],
+                [0, np.sin(np.deg2rad(xangle)), np.cos(np.deg2rad(xangle)),0],
+                [0,0,0,1]])
+    M_RxO = Rx@cam_orig
+    M_Rx = M_x@M_RxO
+    return M_Rx
+    
+def y_rotate_cam(yangle, M_y):
+    M_inv = np.linalg.inv(M_y)
+    cam_orig = np.dot(M_inv, M_y)
+    Ry = np.array([[np.cos(np.deg2rad(yangle)),0, np.sin(np.deg2rad(yangle)),0],
+                    [0,1,0,0],
+                    [-np.sin(np.deg2rad(yangle)), 0, np.cos(np.deg2rad(yangle)),0],
+                    [0,0,0,1]])
+    M_RyO = cam_orig@Ry
+    M_Ry = M_y@M_RyO
+    return M_Ry
+    
+def z_rotate_cam(zangle, M_z):
+    M_inv = np.linalg.inv(M_z)
+    cam_orig = np.dot(M_inv, M_z)
+    Rz = np.array([[np.cos(np.deg2rad(zangle)),-np.sin(np.deg2rad(zangle)),0,0],
+                    [np.sin(np.deg2rad(zangle)),np.cos(np.deg2rad(zangle)),0,0],
+                    [0,0,1,0],
+                    [0,0,0,1]])
+    M_RzO = cam_orig@Rz
+    M_Rz = M_z@M_RzO
+    return M_Rz
+
+# Funções de translação
+def translate(x,y,z):
+    return np.array([[1,0,0,x],
+                        [0,1,0,y],
+                        [0,0,1,z],
+                        [0,0,0,1]])
+    
+
+def translate_cam(x,y,z, M_T):
+    M_inv_T = np.linalg.inv(M_T)
+    cam_orig = np.dot(M_inv_T, M_T)
+    T = np.array([[1,0,0,x],
+                    [0,1,0,y],
+                    [0,0,1,z],
+                    [0,0,0,1]])
+    M_TO = cam_orig@T
+    M_acc = M_T@M_TO
+    return M_acc
+
+# Função emprestada do material da professora
+def draw_arrows(point,base,axis,length=0.5):
+    # Plot vector of x-axis
+    qx = axis.quiver(point[0],point[1],point[2],base[0,0],base[1,0],base[2,0],color='red',pivot='tail',  length=length)
+    # Plot vector of y-axis
+    qy = axis.quiver(point[0],point[1],point[2],base[0,1],base[1,1],base[2,1],color='green',pivot='tail',  length=length)
+    # Plot vector of z-axis
+    qz = axis.quiver(point[0],point[1],point[2],base[0,2],base[1,2],base[2,2],color='blue',pivot='tail',  length=length)
+    return [qx, qy, qz]
+
+# Função para ajustar o eixo de coordenadas do objeto 3D em relação à mudança de posição ou orientação da câmera
+def set_view_object_cam(ax):
+
+    x_lim = ax.get_xlim3d()
+    y_lim = ax.get_ylim3d()
+    z_lim = ax.get_zlim3d()
+
+    x_ran = abs(np.diff(x_lim))
+    x_mean = np.mean(x_lim)
+    y_ran = abs(np.diff(y_lim))
+    y_mean = np.mean(y_lim)
+    z_ran = abs(np.diff(z_lim))
+    z_mean = np.mean(z_lim)
+
+    plot_radius = 0.4 * max([x_ran, y_ran, z_ran])
+
+    # Ajuste para que o grafico não ultrapassar os limites mínimos/maximos
+    min_x, max_x = -30, 30
+    min_y, max_y = -30, 30
+    min_z, max_z = -10, 30
+
+    x0, x1 = x_mean - plot_radius, x_mean + plot_radius
+    y0, y1 = y_mean - plot_radius, y_mean + plot_radius
+    z0, z1 = z_mean - plot_radius, z_mean + plot_radius
+
+    ax.set_xlim3d([min(x0, min_x), max(x1, max_x)])
+    ax.set_ylim3d([min(y0, min_y), max(y1, max_y)])
+    ax.set_zlim3d([min(z0, min_z), max(z1, max_z)])
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
